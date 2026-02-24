d2292fb58be70c67b24ffbd86585e9617ff25cee
youssef1365 <alkarimyoussef@gmail.com>
2026-02-24 20:35:13 +0000

    hhh

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+
+  adapter: vercel()
 });
